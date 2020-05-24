7fb5c5643bcd9948f90d725eb903971b125d0d8d
valoeghese <valoeghesemods@gmail.com>
2020-05-24 22:32:18 +1200

    lessen air resistance down

--- src/main/java/tk/valoeghese/fc0/world/Player.java
+++ src/main/java/tk/valoeghese/fc0/world/Player.java
@@ -51,7 +51,7 @@ public class Player {
 
 	public void tick() {
 		this.velocity.offsetY(this.isSwimming() ? -0.01f : -0.025f);
-		this.velocity.mul(0.85, 0.9, 0.85);
+		this.velocity.mul(0.85, 0.98, 0.85);
 		this.move(this.velocity.getX(), 0.0, 0.0);
 		this.move(0.0, 0.0, this.velocity.getZ());
 
