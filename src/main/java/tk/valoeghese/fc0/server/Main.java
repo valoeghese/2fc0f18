5b6b93f1d8901a7b038d860dcceeebb8b8114a6c
valoeghese <valoeghesemods@gmail.com>
2020-08-31 23:01:34 +1200

    why is this here

--- src/main/java/tk/valoeghese/fc0/server/Main.java
+++ src/main/java/tk/valoeghese/fc0/server/Main.java
@@ -1,7 +1,5 @@
 package tk.valoeghese.fc0.server;
 
-import com.sun.corba.se.spi.activation.Server;
-
 public class Main {
 	private static void main(String[] args) {
 		System.out.println("Starting 2fc0f18 server.");
