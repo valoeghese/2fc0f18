1c8ce894773bca9497455ece0bddb7da64016956
valoeghese <valoeghesemods@gmail.com>
2021-10-12 09:18:50 +1300

    confier

--- src/main/java/tk/valoeghese/fc0/world/gen/generator/TreeGenerator.java
+++ src/main/java/tk/valoeghese/fc0/world/gen/generator/TreeGenerator.java
@@ -1,5 +1,6 @@
 package tk.valoeghese.fc0.world.gen.generator;
 
+import tk.valoeghese.fc0.util.maths.MathsUtils;
 import tk.valoeghese.fc0.world.gen.GenWorld;
 import tk.valoeghese.fc0.world.tile.Tile;
 
@@ -31,47 +32,16 @@ public class TreeGenerator extends Generator<TreeGeneratorSettings> {
 			int z = startZ + rand.nextInt(16);
 			int y = getHeightForGeneration(world, x, z);
 			int height = settings.getHeight(rand);
+			int metaBelow = world.readMeta(x, y - 1, z);
 
-			if (y < 52 || world.readTile(x, y - 1, z) != Tile.GRASS.id || world.readMeta(x, y - 1, z) == 2) {
+			if (y < 52 || world.readTile(x, y - 1, z) != Tile.GRASS.id || metaBelow == 2) {
 				continue;
 			}
 
-			for (int xo = -2; xo <= 2; ++xo) {
-				int totalX = x + xo;
-
-				for (int zo = -2; zo <= 2; ++zo) {
-					int totalZ = z + zo;
-
-					for (int yo = 0; yo < 2; ++yo) {
-						if (yo == 1 && Math.abs(zo) == 2 && Math.abs(xo) == 2 && rand.nextBoolean()) {
-							break;
-						}
-
-						int totalY = y + height + yo;
-
-						if (world.isInWorld(totalX, totalY, totalZ)) {
-							writeTile(world, totalX, totalY, totalZ, Tile.LEAVES.id);
-						}
-					}
-				}
-			}
-
-			int finalCrossY = y + height + 2;
-
-			for (int xo = -1; xo < 2; ++xo) {
-				int totalX = xo + x;
-
-				for (int zo = -1; zo < 2; ++zo) {
-					if (zo != 0 && xo != 0) {
-						continue;
-					}
-
-					int totalZ = zo + z;
-
-					if (world.isInWorld(totalX, finalCrossY, totalZ)) {
-						writeTile(world, totalX, finalCrossY, totalZ, Tile.LEAVES.id);
-					}
-				}
+			if (metaBelow == 1 && rand.nextBoolean()) {
+				this.generateConiferTreeLeaves(world, rand, x, y, z, height);
+			} else {
+				this.generateSimpleTreeLeaves(world, rand, x, y, z, height);
 			}
 
 			for (int yo = 0; yo < height; ++yo) {
@@ -79,6 +49,77 @@ public class TreeGenerator extends Generator<TreeGeneratorSettings> {
 
 				if (world.isInWorld(x, totalY, z)) {
 					world.wgWriteTile(x, totalY, z, Tile.LOG.id);
+				}
+			}
+		}
+	}
+
+	private final void generateConiferTreeLeaves(GenWorld world, Random rand, int x, int y, int z, int height) {
+		if (height + 1 <= 2) { // just in case
+			height = 2;
+		}
+
+		int stage = 0;
+		for (int yo = height + 1; yo >= 2; --yo, stage++) {
+			int thickness = -2; // this is 1 thickness due to fancy maths
+
+			if ((stage & 1) == 1) {
+				thickness = stage / 2; // alternate 1 thickness with 0,1,2 which is treated as 0,2,3 due to the next if and maths
+			}
+
+			if (thickness != 0) {
+				thickness = Math.abs(thickness + 1);
+				int worldY = y + yo;
+
+				for (int xo = -thickness; xo <= thickness; ++xo) {
+					int worldX = x + xo;
+
+					for (int zo = -thickness; zo <= thickness; ++zo) {
+						if (MathsUtils.manhattan(0, 0, xo, zo) <= thickness) {
+							int worldZ = z + zo;
+							writeTile(world, worldX, worldY, worldZ, Tile.LEAVES.id);
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private final void generateSimpleTreeLeaves(GenWorld world, Random rand, int x, int y, int z, int height) {
+		for (int xo = -2; xo <= 2; ++xo) {
+			int totalX = x + xo;
+
+			for (int zo = -2; zo <= 2; ++zo) {
+				int totalZ = z + zo;
+
+				for (int yo = 0; yo < 2; ++yo) {
+					if (yo == 1 && Math.abs(zo) == 2 && Math.abs(xo) == 2 && rand.nextBoolean()) {
+						break;
+					}
+
+					int totalY = y + height + yo;
+
+					if (world.isInWorld(totalX, totalY, totalZ)) {
+						writeTile(world, totalX, totalY, totalZ, Tile.LEAVES.id);
+					}
+				}
+			}
+		}
+
+		int finalCrossY = y + height + 2;
+
+		for (int xo = -1; xo < 2; ++xo) {
+			int totalX = xo + x;
+
+			for (int zo = -1; zo < 2; ++zo) {
+				if (zo != 0 && xo != 0) {
+					continue;
+				}
+
+				int totalZ = zo + z;
+
+				if (world.isInWorld(totalX, finalCrossY, totalZ)) {
+					writeTile(world, totalX, finalCrossY, totalZ, Tile.LEAVES.id);
 				}
 			}
 		}
