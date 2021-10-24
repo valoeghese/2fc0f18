be6626a17f0b733a9824eb41acae287c52aeadab
valoeghese <valoeghesemods@gmail.com>
2021-10-24 23:58:29 +1300

    ok it cmpiles now but crashes on startup :yeehaw:

--- src/main/java/tk/valoeghese/fc0/server/world/ServerChunk.java
+++ src/main/java/tk/valoeghese/fc0/server/world/ServerChunk.java
@@ -1,12 +1,13 @@
 package tk.valoeghese.fc0.server.world;
 
+import tk.valoeghese.fc0.world.GameplayWorld;
 import tk.valoeghese.fc0.world.chunk.Chunk;
 import tk.valoeghese.fc0.world.ChunkAccess;
 
 import javax.annotation.Nullable;
 
 public class ServerChunk extends Chunk {
-	public ServerChunk(ChunkAccess parent, int x, int z, byte[] tiles, byte[] meta, @Nullable int[] kingdoms) {
+	public ServerChunk(GameplayWorld parent, int x, int z, byte[] tiles, byte[] meta, @Nullable int[] kingdoms) {
 		super(parent, x, z, tiles, meta, kingdoms);
 	}
 }
